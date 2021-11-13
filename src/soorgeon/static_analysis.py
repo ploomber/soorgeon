d5f9dc75616cc38b23db9aff5b77adb537eeea9c
Eduardo Blancas Reyes <github@blancas.io>
2021-11-13 14:56:33 -0500

    ignoring names defined from import statements, detecting variables passed as fn args

--- src/soorgeon/static_analysis.py
+++ src/soorgeon/static_analysis.py
@@ -1,4 +1,52 @@
+from functools import reduce
+
 import parso
+
+
+def find_defined_names_from_imports(tree):
+    # build a defined-name -> import-statement-code mapping. Note that
+    # the same code may appear more than once if it defines more than one name
+    # e.g. from package import a, b, c
+    imports = [{
+        name.value: import_.get_code().rstrip()
+        for name in import_.get_defined_names()
+    } for import_ in tree.iter_imports()]
+
+    if imports:
+        imports = reduce(lambda x, y: {**x, **y}, imports)
+    else:
+        imports = {}
+
+    return imports
+
+
+def inside_function_call(leaf):
+    next_sibling = leaf.get_next_sibling()
+
+    try:
+        next_sibling_value = next_sibling.value
+    except AttributeError:
+        next_sibling_value = None
+
+    # ignore names in keyword arguments
+    # e.g., some_function(x=1)
+    # (x does not count since it's)
+    if next_sibling_value == '=':
+        return False
+
+    parent = leaf.parent
+
+    try:
+        left = parent.get_previous_sibling().value == '('
+    except AttributeError:
+        left = False
+
+    try:
+        right = parent.get_next_sibling().value == ')'
+    except AttributeError:
+        right = False
+
+    return left and right
 
 
 def find_inputs_and_outputs(code_str):
@@ -10,20 +58,23 @@ def find_inputs_and_outputs(code_str):
     tree = parso.parse(code_str)
     leaf = tree.get_first_leaf()
 
+    defined_names_from_imports = find_defined_names_from_imports(tree)
+
     inputs, outputs = [], set()
 
     while leaf:
         if leaf.type == 'operator' and leaf.value == '=':
-            next = leaf.get_next_sibling()
+            next_s = leaf.get_next_sibling()
             previous = leaf.get_previous_leaf()
 
             try:
-                children = next.children
+                children = next_s.children
             except AttributeError:
                 inputs_current = []
             else:
                 inputs_current = [
                     e.value for e in children if e.type == 'name'
+                    and e.value not in defined_names_from_imports
                 ]
 
             for variable in inputs_current:
@@ -32,7 +83,15 @@ def find_inputs_and_outputs(code_str):
                 if variable not in outputs:
                     inputs.append(variable)
 
-            outputs.add(previous.value)
+            # ignore keyword arguments, they aren't outputs
+            # e.g. something(key=value)
+            if previous.parent.type != 'argument':
+                outputs.add(previous.value)
+
+        # variables inside function calls are inputs
+        # e.g., some_function(df)
+        elif leaf.type == 'name' and inside_function_call(leaf):
+            inputs.append(leaf.value)
 
         leaf = leaf.get_next_leaf()
 
--- tests/test_static_analysis.py
+++ tests/test_static_analysis.py
@@ -1,3 +1,4 @@
+import parso
 import pytest
 
 from soorgeon import static_analysis
@@ -23,6 +24,18 @@ import pandas as pd
 z = 1
 """
 
+imported_function = """
+from sklearn.datasets import load_iris
+
+# load_iris should be considered an input since it's an imported object
+df = load_iris(as_frame=True)['data']
+"""
+
+# FIXME: another test case but with a class constructor
+input_in_function_call = """
+sns.histplot(df.some_column)
+"""
+
 # ignore classes, functions
 # try assigning a tuple
 
@@ -32,12 +45,16 @@ z = 1
     [simple, {'x', 'y'}, {'z'}],
     [local_inputs, set(), {'x', 'y', 'z'}],
     [imports, set(), {'z'}],
+    [imported_function, set(), {'df'}],
+    [input_in_function_call, {'df'}, set()],
 ],
                          ids=[
                              'only_outputs',
                              'simple',
                              'local_inputs',
                              'imports',
+                             'imported_function',
+                             'input_in_function_call',
                          ])
 def test_find_inputs_and_outputs(code_str, inputs, outputs):
     in_, out = static_analysis.find_inputs_and_outputs(code_str)
@@ -75,3 +92,22 @@ z = y + 1
 ])
 def test_find_upstream(snippets, expected):
     assert static_analysis.find_upstream(snippets) == expected
+
+
+simple_imports = """
+import pandas as pd
+import numpy as np
+"""
+
+
+@pytest.mark.parametrize('code, expected', [
+    [
+        simple_imports, {
+            'np': 'import numpy as np',
+            'pd': '\nimport pandas as pd'
+        }
+    ],
+])
+def test_find_defined_names_from_imports(code, expected):
+    assert static_analysis.find_defined_names_from_imports(
+        parso.parse(code)) == expected
