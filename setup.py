252667d747cb85fafc53ddd247cb06ffed78393b
Eduardo Blancas Reyes <github@blancas.io>
2021-11-13 10:25:26 -0500

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+import re
+import ast
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('src/soorgeon/__init__.py', 'rb') as f:
+    VERSION = str(
+        ast.literal_eval(
+            _version_re.search(f.read().decode('utf-8')).group(1)))
+
+REQUIRES = [
+    'jupytext',
+    'parso',
+    'nbformat',
+    'jinja2',
+    'pyyaml',
+]
+
+DEV = [
+    'pkgmt',
+    'pytest',
+    'yapf',
+    'flake8',
+    'invoke',
+    'twine',
+    'ipython',
+    'ploomber',
+]
+
+setup(name='soorgeon',
+      version=VERSION,
+      description=None,
+      license=None,
+      author=None,
+      author_email=None,
+      url=None,
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      package_data={"": []},
+      classifiers=[],
+      keywords=[],
+      install_requires=REQUIRES,
+      extras_require={
+          'dev': DEV,
+      })
