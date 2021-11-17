3ab4da76178346a7bce4b8ebbeff3456c27d7a2a
Eduardo Blancas Reyes <github@blancas.io>
2021-11-17 14:42:47 -0500

    adds high level description of the refactoring process

--- src/soorgeon/export.py
+++ src/soorgeon/export.py
@@ -1,3 +1,124 @@
+"""
+High-level description of the refactoring process
+
+Given some notebook:
+
+```python
+# H1 header
+
+# H2 header
+x = 1 + 1
+
+# H2 header
+y = x + 1
+
+# H3 header
+z = x + 1
+```
+
+Step 1. Section splitting.
+
+We split it into sections using H2 markdown headers (H1 goes into the first
+task), we call this a proto-task:
+
+
+```python
+# H1 header
+
+# H2 header
+x = 1 + 1
+```
+
+```python
+# H2 header
+y = x + 1
+```
+
+```python
+# H3 header
+z = x + 1
+```
+
+Step 2. Parse inputs and outputs.
+
+Then we figure out which variables each proto-task uses and which ones they
+create:
+
+1. Exposes `x`
+2. Exposes `y`, uses `x`
+3. Exposes `z`, uses `x`
+
+
+Step 3. Resolve dependencies.
+
+Then we resolve dependencies:
+
+1 -> 2
+1 -> 3
+
+
+Step 4. Determine variables to serialize.
+
+Now we determine the products for each proto-task, ignoring the variables that
+aren't used downstream:
+
+1. x
+2. None
+3. None
+
+
+Step 5. Add serialization cell.
+
+Then, we modify each task to serialize the outputs we need:
+
+```python
+# added by soorgeon
+from pathlib import Path
+import pickle
+
+# H1 header
+
+# H2 header
+x = 1 + 1
+
+# added by soorgeon
+Path(product['x']).write_bytes(x)
+```
+
+Step 6. Add parameters cell with upstram dependencies.
+
+We also add the parameters cell with the upstream dependdencies:
+
+```python
+# %% tags=["parameters"]
+upstream = ['one']
+
+# H2 header
+y = x + 1
+```
+
+Step 7. Refactor imports.
+
+We also need to modify imports, we parse all imports in the
+notebooks, delete them, and then add a new cell at the top with the
+subset of import statements that the proto task uses.
+
+```python
+# soorgeon auto-generated imports statements
+import pandas as pd
+
+# %% tags=["parameters"]
+upstream = ['one']
+
+# H2 header
+y = x + 1
+```
+
+Step 8. Generate step.
+
+Finally, we generate the pipeline.yaml file.
+"""
+
 from pathlib import Path
 
 import jupytext
